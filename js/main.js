80aed796de1c0767f03cdaf29226e0cb0f611267
hajh03 <77851770+hajh03@users.noreply.github.com>
2021-04-23 15:58:47 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+
+    new WOW().init();
+
+    
+    // $('').on('click',function(){
+
+    //   $('').animate({marginRight : '0'});500
+
+    // })
+
+ 
+  
+})
+
+
+const thisYear = document.querySelector('.this-year');
+  thisYear.textContent = new Date().getFullYear();
+  
